9e4eab3b2171e2460489797d3b43008eb5b0a50a
tokyobar <tokyobar.us@gmail.com>
2024-07-24 12:17:43 +0000

    test: clinet update

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,6 +6,10 @@ import {
     instagram,
     linkedin,
     twitter,
+    airbnb,
+    binance,
+    coinbase,
+    dropbox,
     send,
     shield,
     star,
@@ -143,3 +147,21 @@ export const socialMedia = [
     },
 ]
 
+export const clients = [
+    {
+        id: 'client-1',
+        logo: airbnb,
+    },
+    {
+        id: 'client-2',
+        logo: binance,
+    },
+    {
+        id: 'client-3',
+        logo: coinbase,
+    },
+    {
+        id: 'client-4',
+        logo: dropbox,
+    },
+]
